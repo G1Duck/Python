6a3266e996d42244f671fc50bff2866ea2194b55
G1Duck <michael@singlesecond.com>
2019-08-22 21:51:52 +0000

    story to python too

---
+++ story.py
@@ -0,0 +1,12 @@
+#  Project Infinity
+import time
+Number = 0
+print("Welcome to the Infinity project!")
+time.sleep(1.5)
+print("How high do you want to go today?")
+Max = int(input())
+while(Number <= Max): 
+	print(Number)
+	Number = Number + 1
+	time.sleep(0.2)
+
